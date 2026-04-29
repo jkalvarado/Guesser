d407f4f6e5aebd1d7ce47f118bd3bf079d57bbb1
Johanna <jkalvarado@csuchico.edu>
2026-04-29 11:41:42 -0700

    Initial test cases

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -15,8 +15,29 @@ class GuesserTest : public ::testing::Test
 };
 
 // Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
+//TEST(GuesserTest, smoke_test)
+//{
+  //Guesser object("Secret");
+  //ASSERT_EQ( 1+1, 2 );
+//}
+
+TEST(GuesserTest, empty_string)
 {
-  Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+	Guesser guess;
+	ASSERT_FALSE(guess.Guesser(string secret("")));
+}
+
+TEST(GuesserTest, over_32){
+	Guesser guess;
+	ASSERT_EQ(36, guess.Guesser("abcdefghijklmnopqrstuvwxyzabcdefghik"));
+}
+
+TEST(GuesserTest, supposed_to_locked){
+	Guesser guess;
+	ASSERT_FALSE(guess.match(0));
+}
+
+TEST(GuesserTest, when_right){
+	Guesser guess;
+	ASSERT_EQ(3, guess.distance());
 }
